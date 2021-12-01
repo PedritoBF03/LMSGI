7d2497ca48d5365868790d11520a0a8cb9284f95
PedritoBF03 <pedrobf2003@gmail.com>
2021-12-01 12:55:31 +0100

    Subida 1/12

--- HTML/3ºRWD/2021/maq2MBFIRTS/js/app.js
+++ HTML/3ºRWD/2021/maq2MBFIRTS/js/app.js
@@ -1,52 +1,47 @@
-console.log ("Cargando app.js")
+console.log ('Cargando app.js');
 const iva = 0.16;
 
 let pcoste = 2000;
 
 const calcularPVP = (precio, impuesto) => {
-    console.log ("saldo = ", pcoste, " €");
-    console.log ("iva = ", iva * 100, " %", "-->", pcoste*impuesto);
-    console.log ("saldo neto =", pcoste * (1 - impuesto), " €");
-    return precio * (1+impuesto);
+    console.log ('Precio de Coste = ', precio, ' €');
+    console.log ('iva = ', impuesto * 100, '%', '-->', precio*impuesto);
+    console.log ('PVP = ', precio * (1 + impuesto), ' €');
+    return precio * (1 + impuesto);
 }
 
-//llamamos a la funcion
-let ptotal = calcularPVP(pcoste, iva);
-console.log (ptotal);
+//llamadas a la función
+//let ptotal = calcularPVP(20, iva);
 
-
-//Funciones de suma
-function sumar1(x,y){
-    return x + y;
+function sumar1 (x,y){
+    // instruccion 1;
+    // instruccion 2;
+    //...
+    //instruccion n;
+    return ( x + y)
 }
 
+/*  solo cuando el cuerpo de la funcion tiene 1 sola instruccion
 const sumar2 = (x, y) => {
     return x + y;
 }
+*/
+const sumar2 = (x, y) =>  x + y ;
 
-let suma = sumar2(3,6);
-console.log (suma)
+let suma = sumar2( 13, 6);
+console.log(suma);
 
-
-
-
-const cubo1 = (x) => {
-    return (x*x*x);
+const cubo1 = ( x ) => {
+    return( x * x * x)
 }
-
 const cubo = (x) => x*x*x;
+console.log( cubo(5,) );
 
-console.log(cubo1(5,2));
-
-
-const potencia = (b,e) => {
-    console.log('hagsdads');
+const potencia = (b, e) => {
     let res = 1;
-    for (i = 1; i <= e; i++){
-        res = res * b;
-        console.log(i);
+    for(i = 1; i <= e; i++){
+        res = res * b
     }
     return res;
 }
-
-console.log(potencia(10,10));
+console.log (potencia (3,3));
